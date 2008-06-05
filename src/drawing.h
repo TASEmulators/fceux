0d84597aa60068b0ff9bd6d0345c1b8795e4db3a
punkrockguy318 <punkrockguy318@users.sf.net>
2008-06-05 11:13:57 +0000

    fixed stupid gcc warning

--- src/drawing.h
+++ src/drawing.h
@@ -5,3 +5,4 @@ void DrawMessage(bool beforeMovie);
 void FCEU_DrawRecordingStatus(uint8* XBuf);
 void FCEU_DrawNumberRow(uint8 *XBuf, int *nstatus, int cur);
 void DrawTextTrans(uint8 *dest, uint32 width, uint8 *textmsg, uint8 fgcolor);
+
