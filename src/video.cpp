d40ffb715ca2980faf561588de3d49b57e559f01
zeromus <zeromus@users.sf.net>
2008-06-06 03:03:14 +0000

    vc8 compile fix

--- src/video.cpp
+++ src/video.cpp
@@ -18,7 +18,10 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#ifndef WIN32
 #include <stdint.h>
+#endif
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
