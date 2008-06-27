e7bf6f1d1693cd8dcbd37fa66d87cb7cd6319c1f
zeromus <zeromus@users.sf.net>
2008-06-27 04:02:16 +0000

    debug prints for rerecord count

--- src/movie.cpp
+++ src/movie.cpp
@@ -925,6 +925,7 @@ bool FCEUMOV_ReadState(std::istream* is, uint32 size)
 			tempMovieData.truncateAt(currFrameCounter);
 			currMovieData = tempMovieData;
 			currMovieData.rerecordCount++;
+			FCEU_DispMessage("State loaded. Rerecord count: %d\n",currMovieData.rerecordCount);
 
 			openRecordingMovie(curMovieFilename);
 			currMovieData.dump(osRecordingMovie, false);
