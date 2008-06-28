508734aee6b18ab27ac621734cac8627309f3234
punkrockguy318 <punkrockguy318@users.sf.net>
2008-06-28 19:19:06 +0000

    Modularized the sdl KeyGrabber

--- get_key.py
+++ get_key.py
@@ -3,15 +3,23 @@ import pygame
 import sys
 from pygame.locals import *
 
-pygame.init()
-width = 300
-height = 100
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Press any key. . .")
 
-while 1:
-  for event in pygame.event.get():
-    if event.type == KEYDOWN:
-      print event.key
-      sys.exit(event.key)
+class KeyGrabber:
+  """ KeyGrabber is a wrapper that gets an SDL key from the user using pygame """
+  def __init__(self, width=300, height=100):
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Press any key. . .")
+
+  def get_key(self):
+    while 1:
+      for event in pygame.event.get():
+        if event.type == KEYDOWN:
+          #print event.key
+          pygame.display.quit()
+          return event.key
+          
+if __name__ == "__main__":
+  kg = KeyGrabber()
+  print kg.get_key()
       
