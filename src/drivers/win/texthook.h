0491f34c1d0e724e3c9027214141d6e044cd3d5a
adelikat <adelikat@users.sf.net>
2008-06-30 15:27:58 +0000

    adding texthooker.cpp & .h

---
+++ src/drivers/win/texthook.h
@@ -0,0 +1,14 @@
+#define NETWORK_ERROR -1
+#define NETWORK_OK     0
+
+extern int TextHookerScanline;
+extern int TextHooker;
+extern uint8 hScroll;
+extern uint8 vScroll;
+extern int callTextHooker;
+
+void TextHookerDoBlit();
+void UpdateTextHooker();
+void KillTextHooker();
+void DoTextHooker();
+void TextHookerCheck();
