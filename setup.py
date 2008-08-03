fc57bd4889a625201d95c38f61dda3391525b972
punkrockguy318 <punkrockguy318@users.sf.net>
2008-08-03 14:30:18 +0000

    gfceu 0.6.1 maintainence release

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(name="gfceu",
-		version="0.6.0",
+		version="0.6.1",
 		scripts = ['gfceu'],
 		data_files=[('share/gfceu/',['gfceu.glade', 'gfceu_big.png', 'gfceu.png']),
                 ('share/pixmaps/', ['gfceu.png']),
                 ('share/man/man1/', ['gfceu.1']),
       			    ('share/applications/', ['gfceu.desktop'])],
 		author = "Lukas Sabota",
-		author_email = "punkrockguy318@comcast.net",
-		url = "http://punkrockguy318.no-ip.org"
+		author_email = "ltsmooth42 _at_ gmail.com",
+		url = "http://dietschnitzel.com/gfceu"
 
 		)
 
