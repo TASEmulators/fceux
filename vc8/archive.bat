10fec606097110ae0b47cb90245a8f5812b042c5
zeromus <zeromus@users.sf.net>
2008-08-04 18:15:03 +0000

    rename the lua examples directory, update changelog, tweak deploy scripts

--- vc8/archive.bat
+++ vc8/archive.bat
@@ -3,4 +3,6 @@ cd ..\output
 ..\vc8\zip -X -9 -r ..\vc8\fceux.zip fceux.exe fceux.chm 7z.dll palettes
 copy /y luapack\luapack.zip ..\vc8\
 ..\vc8\zip -X -9 -r ..\vc8\luapack.zip auxlib.lua
+cd ..\documentation
+..\vc8\zip -X -9 -r ..\vc8\luapack.zip examplelua
 cd ..\vc8
