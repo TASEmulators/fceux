29ae3f917b8c0cec402cb87cf0b3fa81df8a288f
zeromus <zeromus@users.sf.net>
2008-08-13 22:31:48 +0000

    docs reorg

--- htdocs/docs.php
+++ htdocs/docs.php
@@ -11,7 +11,7 @@
  <li>Windows users should look at the CHM which is packaged with the binary, or go <a href=http://fceultra.svn.sourceforge.net/viewvc/fceultra/fceu/src/drivers/win/help/fceux.chm>directly to it</a>; but you cannot open it directly out of IE, as Microsoft feels that this would be a security hazard. You'd better save it first and then unblock it from the file properties.
  <br>Some of the SDL documentation might be informative to Windows users--so if the CHM doesn't answer your question, give it a shot.
  <li>TAS folks might like to look at the <a href="http://fceultra.svn.sourceforge.net/viewvc/fceultra/fceu/documentation/fm2.txt">FM2 spec</a>
- <li>You might be interested in the <a href="docs-fceu.php">old FCE ultra docs</a>, but they are dangerously out of date.
+ <li>You might be interested in the <a href="fceu-docs.php">old FCE ultra docs</a>, but they are dangerously out of date.
 </tr>
 </table>
 
