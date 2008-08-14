7014951029c565d5e6c5bba73492ef8905487690
zeromus <zeromus@users.sf.net>
2008-08-14 15:43:35 +0000

    docs update

--- htdocs/download.php
+++ htdocs/download.php
@@ -18,18 +18,20 @@ SDL port should run any UNIX-type OS (Linux/Solaris/BSD).</li>
     <ul>
      <li>Lua is enabled by use of the [<a href="http://fceux.com/luapack">fceux.com/luapack</a>] which should be unzipped alongside your fceux.exe. 
      <li>Be sure to check out the .chm file. We work pretty hard on keeping that up to date.
+     <li>The latest win32 interim build is generally fresh within a day 
+or two at <a href="http://fceux.com/zip">http://fceux.com/zip</a> -- If you are 
+working with a developer to fix an issue affecting you then this is 
+where you will find your fixed build.
     </ul>
     <li>SDL:</li>
     <ul>
      <li>This build is working, but may have a few problems. Swing by and help out if you want to help shape it up!
      <li><a href=http://scons.org>Scons</a> is required to build from 
 source.</li>
-     <li>Requires SDL, libz, zenity, and liblua5.1. OpenGL is optional, and 
-recommended.</li>
+     <li>Requires SDL, libz, zenity, and liblua5.1. OpenGL is optional, and recommended.</li>
      <li><a href="http://dietschnitzel.com/gfceu">GFceuX</a>, a GTK2 GUI 
 for fceux, is included in the source release.  It is recommened for all non-windows 
 users of the SDL port, unless you particullarly prefer the command-line.</li>
-     <li>There is some controversy over our decision to use the liblua5.1.a set of binaries instead of liblua.a which you get if you build yourself. Be advised that we are considering the issue but that for now you should make symlinks to liblua5.1.a and .so
     </ul>
 </ul>
 </td></tr>
