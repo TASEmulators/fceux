78a310ba093d1457822a2f5589ea78ae78a1e8bf
punkrockguy318 <punkrockguy318@users.sf.net>
2008-12-05 08:18:29 +0000

    shut up some glade warnings and also made pygame optional, but recommended.  Pygame is required for the GUI of the gamepads.  if pygame is not found, the old --inputcfg method is used.

--- get_key.py
+++ get_key.py
@@ -1,25 +1,27 @@
-#!/usr/bin/env python
-import pygame
+#!/usr/bin/env python   
 import sys
-from pygame.locals import *
-
+try:
+    import pygame
+    from pygame.locals import *
+    has_pygame = True
+except ImportError:
+    self.has_pygame = False
 
 class KeyGrabber:
-  """ KeyGrabber is a wrapper that gets an SDL key from the user using pygame """
-  def __init__(self, width=300, height=100):
-    pygame.init()
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption("Press any key. . .")
+    """ KeyGrabber is a wrapper that gets an SDL key from the user using pygame """
+    def __init__(self, width=300, height=100):
+        pygame.init()
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Press any key. . .")
 
-  def get_key(self):
-    while 1:
-      for event in pygame.event.get():
-        if event.type == KEYDOWN:
-          #print event.key
-          pygame.display.quit()
-          return event.key
+    def get_key(self):
+        while 1:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    pygame.display.quit()
+                    return event.key
           
 if __name__ == "__main__":
-  kg = KeyGrabber()
-  print kg.get_key()
+    kg = KeyGrabber()
+    print kg.get_key()
       
