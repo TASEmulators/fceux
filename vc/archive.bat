9396d0021682ea2227c5f3c8ba2f94ab6db66c2c
adelikat <adelikat@users.sf.net>
2009-03-29 13:02:10 +0000

    Win32 - Updated archive.bat to reflect /dll folder revert

--- vc/archive.bat
+++ vc/archive.bat
@@ -2,7 +2,7 @@ del /s fceux.zip
 copy ..\output\fceux.exe .
 upx ..\output\fceux.exe
 cd ..\output
-..\vc\zip -X -9 -r ..\vc\fceux.zip fceux.exe fceux.chm 7z.dll lua5.1.dll palettes dll luaScripts
+..\vc\zip -X -9 -r ..\vc\fceux.zip fceux.exe fceux.chm 7z.dll *.dll palettes luaScripts
 move /y ..\vc\fceux.exe .
 ..\vc\zip -X -9 -r ..\vc\fceux.zip auxlib.lua
 cd ..\
