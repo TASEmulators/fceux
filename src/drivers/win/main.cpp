47ed3844cc64d8e55343b6df6e274199b9a6dba6
zeromus <zeromus@users.sf.net>
2009-11-02 00:02:21 +0000

    maybe fix crash on exit from sloppy sound deinit code

--- src/drivers/win/main.cpp
+++ src/drivers/win/main.cpp
@@ -478,7 +478,7 @@ static void DriverKill(void)
 
 	if(soundo)
 	{
-		TrashSound();
+		TrashSoundNow();
 	}
 
 	CloseWave();
--- src/drivers/win/sound.h
+++ src/drivers/win/sound.h
@@ -3,6 +3,7 @@ extern int32 fps_scale;
 void ConfigSound();
 int InitSound();
 void TrashSound();
+void TrashSoundNow();
 void win_SoundSetScale(int scale);
 void win_SoundWriteData(int32 *buffer, int count);
 void win_Throttle();
