TASEmulators/fceux

commit d95419e83ca9fe1ca414ef133f222e9fb5cf7eb5

GTK: fixed segfault when exiting GetFilename dialog
git-svn-id: svn://svn.code.sf.net/p/fceultra/code/fceu@1744 cd624aef-3799-4ffa-999e-25a118dd8ffa

src/drivers/sdl/input.cpp
@@ -246,13 +246,14 @@ std::string GetFilename(const char* title, bool save, const char* filter)
 	// TODO: make file filters case insensitive	
 	//gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(fileChooser), filterX);
 	gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(fileChooser), filterAll);
-	gtk_dialog_run (GTK_DIALOG (fileChooser));
+	int response = gtk_dialog_run (GTK_DIALOG (fileChooser));
 	
 	// flush gtk events
 	while(gtk_events_pending())
 			gtk_main_iteration_do(TRUE);
 		
-	fname = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (fileChooser));
+	if (response == GTK_RESPONSE_ACCEPT)
+		fname = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (fileChooser));
 
 	gtk_widget_destroy (fileChooser);
 	

commit 2f0651c7cc08650d8e67e1734456660c763de5e9

sdl: fixed a bug introduced in r1744; i think qeed committed changes to an older version of the file or something; all is well now the bug slowed the emulation speed down to 2%

src/drivers/sdl/input.cpp
@@ -379,11 +379,11 @@ KeyboardCommands()
     
     char* movie_fname = "";
     // get the keyboard input
+    #if SDL_VERSION_ATLEAST(1, 3, 0)	 
+    g_keyState = SDL_GetKeyboardState(NULL);	 
+    #else
     g_keyState = SDL_GetKeyState(NULL);
-    if (g_keyState[SDLK_BACKQUOTE])
-        IncreaseEmulationSpeed();
-    else
-        DecreaseEmulationSpeed();
+	#endif
 
     // check if the family keyboard is enabled
     if(InputType[2] == SIFC_FKB) {