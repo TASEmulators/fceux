67a7a2264176003d59b0c5bbb28a2d499f4c9a95
ugetab <ugetab@users.sf.net>
2010-05-08 00:51:14 +0000

    Error test and message added to UPX process. Fixed and commented out documentation addition line, so it can be used if needed, and causes no error message.

--- vc/archive.bat
+++ vc/archive.bat
@@ -1,10 +1,21 @@
 del /s fceux.zip
 copy ..\output\fceux.exe .
 upx ..\output\fceux.exe
+IF ERRORLEVEL 1 GOTO UPXFailed
 cd ..\output
 ..\vc\zip -X -9 -r ..\vc\fceux.zip fceux.exe fceux.chm 7z.dll *.dll palettes luaScripts
 move /y ..\vc\fceux.exe .
 ..\vc\zip -X -9 -r ..\vc\fceux.zip auxlib.lua
-cd ..\
-..\vc\zip -X -9 -r ..\vc\fceux.zip documentation
+cd ..
+REM vc\zip -X -9 -r vc\fceux.zip documentation
 cd vc
+GOTO end
+
+:UPXFailed
+CLS
+echo.
+echo FCEUX.EXE is either compiling or running.
+echo Close it or let it finish before trying this script again.
+pause
+
+:end
