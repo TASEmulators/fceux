8077c10ad6d39dcc12ed42cc362f7e2f70652f01
punkrockguy318 <punkrockguy318@users.sf.net>
2011-04-18 13:53:54 +0000

    sdl: bugfix - list option "nogui" properly as "--nogui" instead of "--no-gui"

--- src/drivers/sdl/sdl.cpp
+++ src/drivers/sdl/sdl.cpp
@@ -118,7 +118,7 @@ Option         Value   Description\n\
 --netkey       s       Use string 's' to create a unique session for the game loaded.\n\
 --players      x       Set the number of local players.\n\
 --rp2mic       {0,1}   Replace Port 2 Start with microphone (Famicom).\n\
---no-gui       {0,1}   Enable or disable the GTK GUI\n";
+--nogui       {0,1}   Enable or disable the GTK GUI\n";
 
 
 // these should be moved to the man file
