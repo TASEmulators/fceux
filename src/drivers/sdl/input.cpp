4059428c2be6c185d13423113d3d9d10edd82783
punkrockguy318 <punkrockguy318@users.sf.net>
2011-10-08 19:49:35 +0000

    sdl: fix mouseclick operations in "--nogui" mode (ie: zapper)

--- src/drivers/sdl/input.cpp
+++ src/drivers/sdl/input.cpp
@@ -720,15 +720,18 @@ GetMouseData(uint32 (&d)[3])
     t = SDL_GetMouseState(&x, &y);
     SDL_Event event;
 #ifdef _GTK
-    // don't ask for gtk mouse info when in fullscreen
-    // we can use sdl directly in fullscreen
-    int fullscreen = 0;
-    g_config->getOption("SDL.Fullscreen", &fullscreen);
-    if(fullscreen == 0)
+    if(noGui == 0)
     {
-      x=GtkMouseData[0];
-      y=GtkMouseData[1];
-      t=GtkMouseData[2];
+      // don't ask for gtk mouse info when in fullscreen
+      // we can use sdl directly in fullscreen
+      int fullscreen = 0;
+      g_config->getOption("SDL.Fullscreen", &fullscreen);
+      if(fullscreen == 0)
+      {
+        x=GtkMouseData[0];
+        y=GtkMouseData[1];
+        t=GtkMouseData[2];
+      }
     }
 #endif
 
