a44ecb49a520c18ba1a6474137d9313dbb64be61
punkrockguy318 <punkrockguy318@users.sf.net>
2011-10-19 08:22:21 +0000

    move fceux-server to fceu directory so that it will be included in tarball and hopefully used -- talk to me in irc if you have an issue with this organazational adjustment

---
+++ fceux-server/types.h
@@ -0,0 +1,38 @@
+/* FCE Ultra Network Play Server
+ *
+ * Copyright notice for this file:
+ *  Copyright (C) 2004 Xodnizel
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __FCEU_TYPES
+#define __FCEU_TYPES
+
+#include <inttypes.h>
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32; 
+
+typedef uint8_t uint8;  
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+
+typedef unsigned long long uint64;
+typedef long long int64;
+#define INLINE inline
+#define GINLINE inline
+
+#endif
