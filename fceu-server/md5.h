cb61de946926aedf19eb987dcb94a9b612b3f795
punkrockguy318 <punkrockguy318@users.sf.net>
2011-10-23 23:02:05 +0000

    moving fceu-server to old/, makes more sense than in fceux source tree
    [[Split portion of a mixed commit.]]

---
+++ fceu-server/md5.h
@@ -0,0 +1,37 @@
+/* FCE Ultra Network Play Server
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
+#ifndef _MD5_H
+#define _MD5_H
+
+struct md5_context
+{
+    uint32 total[2];
+    uint32 state[4];
+    uint8 buffer[64];
+};
+
+void md5_starts( struct md5_context *ctx );
+void md5_update( struct md5_context *ctx, uint8 *input, uint32 length );
+void md5_finish( struct md5_context *ctx, uint8 digest[16] );
+
+/* Uses a static buffer, so beware of how it's used. */
+char *md5_asciistr(uint8 digest[16]);
+
+#endif /* md5.h */
