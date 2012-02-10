80e27fe68132c93134069120db7450273977f0c3
zeromus <zeromus@users.sf.net>
2012-02-10 19:04:48 +0000

    change win32 test build deploy script to reflect new hosting arrangement

--- vc/upload.bat
+++ vc/upload.bat
@@ -1,5 +1,5 @@
-pscp -pw %1 fceux.zip fceux@www.pi-r-squared.com:web/zip/fceux.zip
+pscp -pw %1 fceux.zip fceux@fceux.com:/home/fceux/fceux.com/zip/fceux.zip
 IF NOT EXIST fceux.zip GOTO end
 IF NOT EXIST defaultconfig\makedownload GOTO end
-pscp -pw %1 .\userconfig\download.html fceux@www.pi-r-squared.com:web/web/download.html
+pscp -pw %1 .\userconfig\download.html fceux@fceux.com:home/fceux/fceux.com/web/download.html
 :end
