449ac4352f9d1ed52253a3ae7965f6ac90aa7eec
punkrockguy318 <punkrockguy318@users.sf.net>
2012-08-04 22:33:16 +0000

    updated FSF license in COPYING license file and source and header files

--- src/boards/121.cpp
+++ src/boards/121.cpp
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  * Panda prince pirate.
  * MK4, MK6, A9711 board, MAPPER 187 the same!
  * UNL6035052_Init seems to be the same too, but with prot array in reverse
--- src/boards/t-227-1.cpp
+++ src/boards/t-227-1.cpp
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 // T-227-1, 820632, MMC3 based, multimenu, 60000in1 (0010) dip switches
