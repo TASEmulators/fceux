d5cb50be9ac88d1170ba57273590786a85810a79
ansstuff <ansstuff@users.sf.net>
2012-08-28 15:23:05 +0000

    updated SubWCRev
    [[Split portion of a mixed commit.]]

---
+++ vc/SubWCRev.bat
@@ -0,0 +1,4 @@
+SubWCRev.exe ..\.. "defaultconfig\svnrev_template.h" "userconfig\svnrev.h"
+IF NOT EXIST defaultconfig\makedownload exit 0
+SubWCRev.exe ..\.. "..\..\web\download.html" "userconfig\download.html"
+exit 0
--- vc/defaultconfig/SubWCRev.bat
+++
@@ -1,4 +0,0 @@
-SubWCRev.exe ..\.. "svnrev_template.h" "..\userconfig\svnrev.h"
-IF NOT EXIST makedownload exit 0
-SubWCWeb.exe ..\.. "..\..\..\web\download.html" "..\userconfig\download.html"
-exit 0
