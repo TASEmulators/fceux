4415f95227761c93b0036e207bd42ff55d575bc8
punkrockguy318 <punkrockguy318@users.sf.net>
2012-11-02 13:48:38 +0000

    sdl: fix a potential security issue with loading the game genie rom (thank you Joe Mahmias for your patch!)

--- src/drivers/sdl/gui.cpp
+++ src/drivers/sdl/gui.cpp
@@ -1663,7 +1663,7 @@ void loadGameGenie ()
 		GtkWidget* d;
 		enableGameGenie(TRUE);
 		d = gtk_message_dialog_new(GTK_WINDOW(MainWindow), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, 
-			("Game Genie ROM copied to " + fn_out).c_str());
+     		"Game Genie ROM copied to: '%s'.", fn_out.c_str());
 		gtk_dialog_run(GTK_DIALOG(d));
 		gtk_widget_destroy(d);
 
