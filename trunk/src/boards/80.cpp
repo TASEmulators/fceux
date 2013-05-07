80ec97d159c9bdb6d5a0db87978d56350336ea57
CaH4e3 <cah4e3@users.sf.net>
2013-05-07 05:49:08 +0000

    copyrights

--- trunk/src/boards/80.cpp
+++ trunk/src/boards/80.cpp
@@ -2,6 +2,7 @@
  *
  * Copyright notice for this file:
  *  Copyright (C) 2012 CaH4e3
+ *  Copyright (C) 2002 Xodnizel
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
