TASEmulators/fceux

commit 79b3c954cad12ebd53c90309608ebcadcc215bff

fix extended attributes in newppu better, and fix old ppu's chr banking to match newppu maybe?
git-svn-id: svn://svn.code.sf.net/p/fceultra/code/fceu/trunk@3360 cd624aef-3799-4ffa-999e-25a118dd8ffa

src/boards/mmc5.cpp
@@ -27,6 +27,7 @@
 #define SpriteON    (PPU[1] & 0x10)	//Show Sprite
 #define ScreenON    (PPU[1] & 0x08)	//Show screen
 #define PPUON       (PPU[1] & 0x18)	//PPU should operate
+#define Sprite16    (PPU[0] & 0x20)	//Sprites 8x16/8x8
 
 static void (*sfun)(int P);
 static void (*psfun)(void);
@@ -113,9 +114,9 @@ typedef struct __cartdata {
 	uint8 size;
 } cartdata;
 
-#define Sprite16  (PPU[0]& 0x20)   //Sprites 8x16/8x8
-//#define MMC5SPRVRAMADR(V)      &MMC5SPRVPage[(V)>>10][(V)]
-static inline uint8 *  MMC5BGVRAMADR(uint32 A) {
+
+uint8* MMC5BGVRAMADR(uint32 A)
+{
 	if(Sprite16)
 	{
 		bool isPattern = PPUON;
@@ -235,12 +236,20 @@ uint8 FASTCALL mmc5_PPURead(uint32 A)
 				return ExRAM[A & 0x3FF];
 			}
 		}
-
+		
 		if (MMC5HackCHRMode == 1)
 		{
 			if((A&0x3FF)>=0x3C0)
 			{
-				return ExRAM[NTRefreshAddr & 0x3ff];
+				uint8 byte = ExRAM[NTRefreshAddr & 0x3ff];
+				//get attribute part
+				byte >>= 6;
+				//adjust it to the right position within the byte
+				int x = NTRefreshAddr&31;
+				int y = NTRefreshAddr>>5;
+				if(y&2) byte <<= 4;
+				if(x&2) byte <<= 2;
+				return byte;
 			}
 		}
 			

src/ppu.cpp
@@ -374,18 +374,7 @@ uint8 UPALRAM[0x03];//for 0x4/0x8/0xC addresses in palette, the ones in
 #define MMC5SPRVRAMADR(V)   &MMC5SPRVPage[(V) >> 10][(V)]
 #define VRAMADR(V)          &VPage[(V) >> 10][(V)]
 
-//mbg 8/6/08 - fix a bug relating to
-//"When in 8x8 sprite mode, only one set is used for both BG and sprites."
-//in mmc5 docs
-uint8 * MMC5BGVRAMADR(uint32 V) {
-	if (!Sprite16) {
-		extern uint8 mmc5ABMode;				/* A=0, B=1 */
-		if (mmc5ABMode == 0)
-			return MMC5SPRVRAMADR(V);
-		else
-			return &MMC5BGVPage[(V) >> 10][(V)];
-	} else return &MMC5BGVPage[(V) >> 10][(V)];
-}
+extern uint8* MMC5BGVRAMADR(uint32 A);
 
 //this duplicates logic which is embedded in the ppu rendering code
 //which figures out where to get CHR data from depending on various hack modes
@@ -500,7 +489,7 @@ void (*FFCEUX_PPUWrite)(uint32 A, uint8 V) = 0;
 
 #define CALL_PPUWRITE(A, V) (FFCEUX_PPUWrite ? FFCEUX_PPUWrite(A, V) : FFCEUX_PPUWrite_Default(A, V))
 
-//whether to use the new ppu (new PPU doesn't handle MMC5 extra nametables at all
+//whether to use the new ppu
 int newppu = 0;
 
 void ppu_getScroll(int &xpos, int &ypos) {

src/ppu.h
@@ -22,6 +22,7 @@ void FCEUPPU_SaveState(void);
 void FCEUPPU_LoadState(int version);
 uint32 FCEUPPU_PeekAddress();
 uint8* FCEUPPU_GetCHR(uint32 vadr, uint32 refreshaddr);
+int FCEUPPU_GetAttr(int ntnum, int xt, int yt);
 void ppu_getScroll(int &xpos, int &ypos);
 
 

commit 9ff8ecb8564ad6f94a84096fa90e4cd8b8de507b

fix Uchuu Keibitai SDF in oldppu (broken in r3360)

src/boards/mmc5.cpp
@@ -122,25 +122,25 @@ uint8* MMC5BGVRAMADR(uint32 A)
 	{
 		if(Sprite16)
 		{
-			bool isPattern = !!PPUON;
-			if (newppu && ppuphase == PPUPHASE_OBJ && isPattern)
+			bool isPattern = PPUON;
+			if (ppuphase == PPUPHASE_OBJ && isPattern)
 				return &ABANKS[(A) >> 10][(A)];
-			if (newppu && ppuphase == PPUPHASE_BG && isPattern)
+			if (ppuphase == PPUPHASE_BG && isPattern)
 				return &BBANKS[(A) >> 10][(A)];
 			else if(mmc5ABMode == 0)
 				return &ABANKS[(A) >> 10][(A)];
 			else 
 				return &BBANKS[(A) >> 10][(A)];
 		}
-		else return &ABANKS[(A) >> 10][(A)];
+		else return &ABANKS[(A) >> 10][(A)];;
 	}
 
 	if (!Sprite16) {
 		if (mmc5ABMode == 0)
-			return MMC5SPRVRAMADR(A);
+			return &ABANKS[(A) >> 10][(A)];
 		else
-			return &MMC5BGVPage[(A) >> 10][(A)];
-	} else return &MMC5BGVPage[(A) >> 10][(A)];
+			return &BBANKS[(A) >> 10][(A)];
+	} else return &BBANKS[(A) >> 10][(A)];
 }
 
 static void mmc5_PPUWrite(uint32 A, uint8 V) {

src/ppu.cpp
@@ -374,7 +374,7 @@ uint8 UPALRAM[0x03];//for 0x4/0x8/0xC addresses in palette, the ones in
 #define MMC5SPRVRAMADR(V)   &MMC5SPRVPage[(V) >> 10][(V)]
 #define VRAMADR(V)          &VPage[(V) >> 10][(V)]
 
-extern uint8* MMC5BGVRAMADR(uint32 A);
+uint8* MMC5BGVRAMADR(uint32 A);
 
 //this duplicates logic which is embedded in the ppu rendering code
 //which figures out where to get CHR data from depending on various hack modes
@@ -770,7 +770,7 @@ static DECLFR(A2007) {
 
 					if (debug_loggingCD)
 						LogAddress = GetCHRAddress(tmp);
-					if(MMC5Hack)
+					if(MMC5Hack && newppu)
 						VRAMBuffer = *MMC5BGVRAMADR(tmp);
 					else
 						VRAMBuffer = VPage[tmp >> 10][tmp];