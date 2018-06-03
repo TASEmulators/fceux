7552f4404b0071db5a8b9781e45408e2e185a713
Brad Smith <bbbradsmith@users.noreply.github.com>
2018-06-03 03:05:16 -0400

    > 0 to != NULL?
    The compiler error seems to be from the uncommon phrasing of > 0 instead of the well known != NULL, posing that as a more straightforward alternative.

--- fceux-server/server.cpp
+++ fceux-server/server.cpp
@@ -114,7 +114,7 @@ int LoadConfigFile(char *fn)
 	if(fp=fopen(fn,"rb"))
 	{
 		char buf[256];
-		while(fgets(buf, 256, fp) > (void *)0)
+		while(fgets(buf, 256, fp) != NULL)
 		{
 			if(!strncasecmp(buf,"maxclients",strlen("maxclients")))
 				sscanf(buf,"%*s %d",&ServerConfig.MaxClients);
