f162600f5476cd677f2f546e1b4d4e756206e31d
Matthew Budd <mjbudd77@gmail.com>
2020-05-14 07:55:57 -0400

    Added clean before build in pipeline.

--- azure/linux_build.sh
+++ azure/linux_build.sh
@@ -18,6 +18,7 @@ sudo apt-get install libgtk-3-dev
 sudo apt-get install libgtksourceview-3.0-dev
 sudo apt-get install scons
 
+scons   --clean
 scons   GTK3=1   SYSTEM_LUA=1   SYSTEM_MINIZIP=1   CREATE_AVI=1
 
 ls -ltr ./bin/*
