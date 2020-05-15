7fb5ed52c4795994f1974d6fb3b6dd7923504b4e
Matthew Budd <mjbudd77@gmail.com>
2020-05-14 20:04:49 -0400

    Removed --fix-missing references from pipeline script. The issue with the pipeline lies on the microsoft side with there linux package servers. There is nothing that can be done here.

--- azure/linux_build.sh
+++ azure/linux_build.sh
@@ -10,13 +10,12 @@ python3 --version
 
 sudo apt-get install lua5.1-dev
 pkg-config --cflags --libs  lua5.1
-sudo apt-get install libsdl1.2-dev  --fix-missing
+sudo apt-get install libsdl1.2-dev  
 sdl-config --cflags --libs
-sudo apt-get install libsdl2-dev  --fix-missing
+sudo apt-get install libsdl2-dev  
 sdl2-config --cflags --libs
 sudo apt-get install libminizip-dev
 pkg-config --cflags --libs  minizip
-#sudo apt-get install libxml2-dev
 #sudo apt-get install libgtk2.0-dev
 sudo apt-get install libgtk-3-dev
 pkg-config --cflags --libs  gtk+-3.0
