a5ecc0fb461f5350bf02cd5b8ac3de81f17c0626
bbbradsmith <bbbradsmith@users.noreply.github.com>
2020-06-17 03:09:57 -0400

    win64 zip archive files from other folders need directory stripped

--- pipelines/win64_build.bat
+++ pipelines/win64_build.bat
@@ -7,8 +7,11 @@ msbuild %PROJECT_ROOT%\vc\vc14_fceux.vcxproj /p:Configuration=Release /p:Platfor
 cd %PROJECT_ROOT%\vc
 
 REM Create Zip Archive
+
 cd %PROJECT_ROOT%\output
-..\vc\zip -X -9 -r ..\vc\fceux64.zip ..\vc\x64\Release\fceux.exe fceux.chm taseditor.chm ..\src\drivers\win\lua\x64\lua5.1.dll ..\src\drivers\win\lua\x64\lua51.dll 7z.dll ..\src\auxlib\auxlib.lua palettes luaScripts tools
+..\vc\zip -X -9 -j ..\vc\fceux64.zip ..\vc\x64\Release\fceux.exe ..\src\drivers\win\lua\x64\lua5.1.dll ..\src\drivers\win\lua\x64\lua51.dll ..\src\auxlib.lua
+@if ERRORLEVEL 1 goto end
+..\vc\zip -X -9 -u -r ..\vc\fceux64.zip fceux.chm taseditor.chm palettes luaScripts tools 7z.dll
 @if ERRORLEVEL 1 goto end
 
 cd %PROJECT_ROOT%
