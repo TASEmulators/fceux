be6dff9d68897653b7251aa60267b2e8b081638c
Matt Budd <mjbudd77@gmail.com>
2020-06-16 21:23:52 -0400

    Added 7z.dll to zipped package file.

--- pipelines/win32_build.bat
+++ pipelines/win32_build.bat
@@ -7,8 +7,9 @@ msbuild %PROJECT_ROOT%\vc\vc14_fceux.vcxproj
 
 cd %PROJECT_ROOT%\vc
 
-REM Copy Executable to output directory
+REM Copy Executable and dlls to output directory
 copy vc14_bin_Debug\fceux.exe  ..\output\.
+copy vc14_bin_Debug\7z.dll     ..\output\.
 
 REM Create Zip Archive
 archive.bat
