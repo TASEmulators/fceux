f2dc2c5755d5ba05f05be5e397594ee9846e72fd
Matthew Budd <mjbudd77@gmail.com>
2020-07-11 20:04:58 -0400

    Added debug statements to genGitHdr script.

--- scripts/genGitHdr.sh
+++ scripts/genGitHdr.sh
@@ -17,14 +17,17 @@ echo "#define FCEUX_GIT_REV  \"$GIT_REV\" " >> $TMP_FILE;
 echo "const char *fceu_get_git_url(void){ return FCEUX_GIT_URL; }" >> $TMP_FILE
 echo "const char *fceu_get_git_rev(void){ return FCEUX_GIT_REV; }" >> $TMP_FILE
 
+echo "Git URL: $GIT_URL ";
+echo "Git Rev: $GIT_REV ";
 if [ -e $OUTPUT_DIR/$FILE ]; then
 
-	diff $TMP_FILE  $OUTPUT_DIR/$FILE
+	diff -q $TMP_FILE  $OUTPUT_DIR/$FILE
 
 	if [ $? != 0 ]; then
-		echo 'File Differs';
-		mv -f  $TMP_FILE  $OUTPUT_DIR/$FILE
+		mv -f  $TMP_FILE  $OUTPUT_DIR/$FILE;
+		echo "Updated $OUTPUT_DIR/$FILE";
 	fi
 else
 	mv -f  $TMP_FILE  $OUTPUT_DIR/$FILE
+	echo "Generated $OUTPUT_DIR/$FILE";
 fi
