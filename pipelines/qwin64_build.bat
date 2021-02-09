fdba5abb3966f8c4245b897a1d3b59743e086dc8
mjbudd77 <mjbudd77@gmail.com>
2021-02-08 23:33:21 -0500

    First successful build of Windows Qt/SDL on appveyor.

--- pipelines/qwin64_build.bat
+++ pipelines/qwin64_build.bat
@@ -1,12 +1,28 @@
 
-rmdir /q /s build
+set PROJECT_ROOT=%~dp0..
+set CWD=%CD%
+
+call "C:\Qt\5.15.2\msvc2019_64\bin\qtenv2.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+cd %CWD%
+
+where cmake
+where nmake
+where msbuild
+REM where zip
+REM where unzip
+where windeployqt
+
+REM rmdir /q /s build
 
 mkdir build
 cd    build
+mkdir bin
 
-curl -LO http://www.libsdl.org/release/SDL2-devel-2.0.14-VC.zip
+curl -s -LO http://www.libsdl.org/release/SDL2-devel-2.0.14-VC.zip
 
-rmdir /q /s SDL2
+REM rmdir /q /s SDL2
 
 powershell -command "Expand-Archive" SDL2-devel-2.0.14-VC.zip .
 
@@ -14,12 +30,31 @@ rename SDL2-2.0.14  SDL2
 
 set SDL_INSTALL_PREFIX=%CD%
 
-cmake -G "NMake Makefiles" -DSDL_INSTALL_PREFIX=%SDL_INSTALL_PREFIX%  ..
+REM cmake -h
+REM cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DSDL_INSTALL_PREFIX=%SDL_INSTALL_PREFIX%  ..
+cmake -DSDL_INSTALL_PREFIX=%SDL_INSTALL_PREFIX%  ..
 
-nmake
+REM nmake
+msbuild /m fceux.sln /p:Configuration=Release
+@if ERRORLEVEL 1 goto end
 
-copy %SDL_INSTALL_PREFIX%\SDL2\lib\x64\SDL2.dll  src\.
+copy src\Release\fceux.exe bin\qfceux.exe
+copy %SDL_INSTALL_PREFIX%\SDL2\lib\x64\SDL2.dll  bin\.
 
-windeployqt src/fceux.exe
+windeployqt bin\qfceux.exe
 
-cd ..
+dir bin
+
+REM Create Zip Archive
+%PROJECT_ROOT%\vc\zip  -X -9 -r %PROJECT_ROOT%\vc\qfceux64.zip  bin
+@if ERRORLEVEL 1 goto end
+
+cd %PROJECT_ROOT%\output
+%PROJECT_ROOT%\vc\zip -X -9 -u -r %PROJECT_ROOT%\vc\qfceux64.zip fceux.chm taseditor.chm palettes luaScripts tools
+@if ERRORLEVEL 1 goto end
+
+cd %PROJECT_ROOT%
+
+appveyor  PushArtifact  %PROJECT_ROOT%\vc\qfceux64.zip
+
+:end
