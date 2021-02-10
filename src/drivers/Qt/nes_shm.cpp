ea8fbbf7747add6994d36e243f17247d6234cc3a
mjbudd77 <mjbudd77@gmail.com>
2021-02-09 20:30:47 -0500

    Removed unnecessary headers from nes_shm.cpp.

--- src/drivers/Qt/nes_shm.cpp
+++ src/drivers/Qt/nes_shm.cpp
@@ -20,13 +20,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
 
 #include "Qt/nes_shm.h"
 
