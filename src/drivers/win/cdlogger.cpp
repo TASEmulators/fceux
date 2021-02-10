TASEmulators/fceux

commit 30fcc91fdc2e308567c0ad6cf4042f37d5af8bfb

Customizing Hex Editor and Debugger Highlight Colors (#310)
* Configurable Hex Editor text colors.
* Add a customizable color for address title.
* 1. Add some more colors to Debugger highlight
2. Sorting all the colors out for future customization.
3. Re-arranged some includes.
* Make the debugger highlight color customizable.
* Move the button to the right and make the default window size wider.
* Adding color icons and hex values to the color menu items.
* 1. Enlarge the color icon a few pixels
* Debugger and Hex Editor shares the same custom color buffer.
This function finally completed. I believe there is no more details can be developed in recent period.
* 1. Add icon to color menu in another way to prevent the odd space between icons and text.
2. Free some malloc() resources when exit, though it would be somehow eventually recycled by system, but that's not a good rabbit.
3. GetROMName and GetROMPath has malloc() and called so many times in the code, it seems there's no any free() call for their returned char*, it's OK to make them directly return a std::string which is more comfortable for use.
* I forgot hDebug is still NULL in WM_INITDIALOG.
* 1. Rewrite debugger resizing logic, now all the controls on the dialog can stretch and shrink with the window size, when you enlarge the window, the controls on the right is also stretched to fit the height.
2. Moved Syntax highlight and Default window size button to the menu bar, in the past, if the window size is too small to show the default size button on the bottom right corner, it can't be pressed to restore the window, but now you can press it anytime.
* Changed ID of color menu to prevent conflict of some existing dialog controls.
* I don't know why restore default window size cannot be done before game loaded.
* 1. Fix Debugger default width with IDA font.
2. Fix Hex Editor single byte highlight in red.

src/drivers/win/cdlogger.cpp
@@ -23,7 +23,6 @@
 #include "../../cart.h" //mbg merge 7/18/06 moved beneath fceu.h
 #include "../../x6502.h"
 #include "../../debug.h"
-#include "debugger.h"
 #include "tracer.h"
 #include "cdlogger.h"
 #include "main.h" //for GetRomName()
@@ -116,7 +115,7 @@ INT_PTR CALLBACK CDLoggerCallB(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lP
 			if (autoloadCDL)
 			{
 				char nameo[2048];
-				strcpy(nameo, GetRomPath());
+				strcpy(nameo, GetRomPath().c_str());
 				strcat(nameo, mass_replace(GetRomName(), "|", ".").c_str());
 				strcat(nameo, ".cdl");
 				LoadCDLog(nameo);
@@ -289,8 +288,8 @@ void SaveCDLogFile()
 	if (loadedcdfile[0] == 0)
 	{
 		char nameo[2048];
-		strcpy(nameo, GetRomPath());
-		strcat(nameo, mass_replace(GetRomName(), "|", ".").c_str());
+		strcpy(nameo, GetRomName().c_str());
+		strcat(nameo, mass_replace(GetRomPath(), "|", ".").c_str());
 		strcat(nameo, ".cdl");
 		RenameCDLog(nameo);
 	}
@@ -500,7 +499,7 @@ void CDLoggerROMChanged()
 
 	// try to load respective CDL file
 	char nameo[2048];
-	strcpy(nameo, GetRomPath());
+	strcpy(nameo, GetRomName().c_str());
 	strcat(nameo, mass_replace(GetRomName(), "|", ".").c_str());
 	strcat(nameo, ".cdl");
 

src/drivers/win/cheat.cpp
@@ -22,7 +22,6 @@
 #include "cheat.h"
 #include "memview.h"
 #include "memwatch.h"
-#include "debugger.h"
 #include "ramwatch.h"
 #include "../../fceu.h"
 #include "../../cart.h"
@@ -401,6 +400,7 @@ INT_PTR CALLBACK CheatConsoleCallB(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARA
 		case WM_QUIT:
 		case WM_CLOSE:
 			DestroyWindow(hCheatTip);
+			DestroyMenu(hCheatcontext);
 			if (CheatStyle)
 				DestroyWindow(hwndDlg);
 			else

src/drivers/win/config.cpp
@@ -116,34 +116,12 @@ extern int GGConv_wndx, GGConv_wndy;
 extern int MetaPosX,MetaPosY;
 extern int MLogPosX,MLogPosY;
 
+// owomomo: I'm tired to write those repeated words
+#define ACRGB(name) AC(name##Color##R), AC(name##Color##G), AC(name##Color##B)
+#define ACOPRGB(_OP) OPHEXRGB(_OP, _COMMA), OPCDLRGB(_OP, _COMMA), OPDBGRGB(_OP, _COMMA)
+extern COLORREF custom_color[];
+
 extern int HexRowHeightBorder;
-extern int HexBackColorR;
-extern int HexBackColorG;
-extern int HexBackColorB;
-extern int HexForeColorR;
-extern int HexForeColorG;
-extern int HexForeColorB;
-extern int HexHlBackColorR;
-extern int HexHlBackColorG;
-extern int HexHlBackColorB;
-extern int HexHlForeColorR;
-extern int HexHlForeColorG;
-extern int HexHlForeColorB;
-extern int HexFreezeColorR;
-extern int HexFreezeColorG;
-extern int HexFreezeColorB;
-extern int RomFreezeColorR;
-extern int RomFreezeColorG;
-extern int RomFreezeColorB;
-extern int HexBoundColorR;
-extern int HexBoundColorG;
-extern int HexBoundColorB;
-extern int HexHlShdBackColorR;
-extern int HexHlShdBackColorG;
-extern int HexHlShdBackColorB;
-extern int HexHlShdForeColorR;
-extern int HexHlShdForeColorG;
-extern int HexHlShdForeColorB;
 extern int importBookmarkProps;
 
 //adelikat:  Hacky fix for Ram Watch recent menu
@@ -457,34 +435,24 @@ static CFGSTRUCT fceuconfig[] =
 	AC(fullscreenByDoubleclick),
 	AC(CurrentState),
 	AC(HexRowHeightBorder),
-	AC(HexBackColorR),
-	AC(HexBackColorG),
-	AC(HexBackColorB),
-	AC(HexForeColorR),
-	AC(HexForeColorG),
-	AC(HexForeColorB),
-	AC(HexHlBackColorR),
-	AC(HexHlBackColorG),
-	AC(HexHlBackColorB),
-	AC(HexHlForeColorR),
-	AC(HexHlForeColorG),
-	AC(HexHlForeColorB),
-	AC(HexHlShdBackColorR),
-	AC(HexHlShdBackColorG),
-	AC(HexHlShdBackColorB),
-	AC(HexHlShdForeColorR),
-	AC(HexHlShdForeColorG),
-	AC(HexHlShdForeColorB),
-	AC(HexFreezeColorR),
-	AC(HexFreezeColorG),
-	AC(HexFreezeColorB),
-	AC(RomFreezeColorR),
-	AC(RomFreezeColorG),
-	AC(RomFreezeColorB),
-	AC(HexBoundColorR),
-	AC(HexBoundColorG),
-	AC(HexBoundColorB),
 	AC(importBookmarkProps),
+	ACOPRGB(ACRGB),
+	AC(custom_color[0]),
+	AC(custom_color[1]),
+	AC(custom_color[2]),
+	AC(custom_color[3]),
+	AC(custom_color[4]),
+	AC(custom_color[5]),
+	AC(custom_color[6]),
+	AC(custom_color[7]),
+	AC(custom_color[8]),
+	AC(custom_color[9]),
+	AC(custom_color[10]),
+	AC(custom_color[11]),
+	AC(custom_color[12]),
+	AC(custom_color[13]),
+	AC(custom_color[14]),
+	AC(custom_color[15]),
 	//ACS(memwLastfile[2048]),
 
 	AC(AutoRWLoad),

src/drivers/win/debugger.h
@@ -69,7 +69,112 @@ public:
 
 } *debugSystem;
 
-// extern WNDPROC DefaultEditCtrlProc;
-// extern INT_PTR APIENTRY FilterEditCtrlProc(HWND hwnd, UINT msg, WPARAM wP, LPARAM lP);
+// Partial List of Color Definitions
+// owomomo: I'm tired to write those repeated words.
+#define OPHEXRGB(_OP, _SEP)                              \
+/* normal hex text color */                              \
+_OP(HexFore,         0,   0,   0) _SEP /* Black */       \
+/* normal hex background color */                        \
+_OP(HexBack,       255, 255, 255) _SEP /* White */       \
+/* highlight hex text color */                           \
+_OP(HexHlFore,     255, 255, 255) _SEP /* White */       \
+/* highlight hex background color */                     \
+_OP(HexHlBack,       0,   0,   0) _SEP /* Black */       \
+/* unfocused highlight hex text color */                 \
+_OP(HexHlShdFore,    0,   0,   0) _SEP /* Black */       \
+/* unfocused highlight hex background color */           \
+_OP(HexHlShdBack,  224, 224, 224) _SEP /* Grey */        \
+/* freezed color */                                      \
+_OP(HexFreeze,       0,   0, 255) _SEP /* Blue */        \
+/* freezed color in ROM */                               \
+_OP(RomFreeze,     255,   0,   0) _SEP /* Red */         \
+/* hex color out of bounds */                            \
+_OP(HexBound,      220, 220, 220) _SEP /* Grey */        \
+/* bookmark color */                                     \
+_OP(HexBookmark,     0, 204,   0) _SEP /* Light green */ \
+/* address header color*/                                \
+_OP(HexAddr,       128, 128, 128)      /* Dark grey */
+
+#define OPCDLRGB(_OP, _SEP)                              \
+/* Logged as code */                                     \
+_OP(CdlCode,       160, 140,   0) _SEP /* Dark yellow */ \
+/* Logged as data */                                     \
+_OP(CdlData,         0,   0, 210) _SEP /* Blue */        \
+/* Logged as PCM */                                      \
+_OP(CdlPcm,          0, 130, 160) _SEP /* Cyan */        \
+/* Logged as code and data */                            \
+_OP(CdlCodeData,     0, 190,   0) _SEP /* Green */       \
+/* Rendered */                                           \
+_OP(CdlRender,     210, 190,   0) _SEP /* Yellow */      \
+/* Read */                                               \
+_OP(CdlRead,        15,  15, 255) _SEP /* Light blue */  \
+/* Rendered and read */                                  \
+_OP(CdlRenderRead,   5, 255,   5)      /* Light green */
+
+// Debugger color list
+// owomomo: the configurable colors
+#define OPDBGRGB(_OP, _SEP)                        \
+/* PC */                                           \
+_OP(DbgPC,       0,   0, 255) _SEP /* Blue */      \
+/* Mnemonic */                                     \
+_OP(DbgMnem,     0,   0, 128) _SEP /* Dark blue */ \
+/* Operand */                                      \
+_OP(DbgOper,     0, 128,   0) _SEP /* Green */     \
+/* Comment */                                      \
+_OP(DbgComm,   128, 128, 128) _SEP /* Grey */      \
+/* Operand note */                                 \
+_OP(DbgOpNt,   128, 128, 255) _SEP /* Purple */    \
+/* Symbolic name */                                \
+_OP(DbgSym,     34,  75, 143) _SEP /* Rurikon */   \
+/* Effective address */                            \
+_OP(DbgEff,    106, 109, 169) _SEP /* Fujinando */ \
+/* RTS */                                          \
+_OP(DbgRts,    187,  80,  93)      /* Imayou */
+
+#define _COMMA ,
+#define SBT(name, suf) name##suf
+
+#define SBCLR(name, suf) SBT(name, Color)##suf
+#define SPCLR(pf, name, suf) pf SBCLR(name, suf)
+#define CSCLR(pf, name, suf, op, val) SPCLR(pf, name, suf) op val
+#define CNRGB(pf, name, op, r, g, b, sep) CSCLR(pf, name, R, op, r) sep CSCLR(pf, name, G, op, g) sep CSCLR(pf, name, B, op, b)
+#define PPRGB(name) NULL, CNRGB(&, name, , , , , _COMMA)
+#define MKRGB(name) (RGB(SBCLR(name, R), SBCLR(name, G), SBCLR(name, B)))
+#define DEFRGB(name, r, g, b) CNRGB( , name, =, r, g, b, _COMMA)
+#define DCLRGB(name, r, g, b) CNRGB( , name, , , , , _COMMA)
+#define CMPRGB(name, r, g, b) (CNRGB( , name, !=, r, g, b, ||))
+
+#define SBCF(name) SBT(name, ChFmt)
+#define PPCF(name) &SBCF(name)
+#define PPCCF(name) PPRGB(name), PPCF(name)
+#define DEFCF(name, r, g, b) (DEFRGB(name, r, g, b), SBCF(name).crTextColor = RGB(r, g, b))
+#define CMPCF(name, r, g, b) (CNRGB( , name, !=, r, g, b, ||) || SBCF(name).crTextColor != RGB(r, g, b))
+#define INITCF(name)                        \
+(memset(PPCF(name), 0, sizeof(SBCF(name))), \
+SBCF(name).cbSize = sizeof(SBCF(name)),     \
+SBCF(name).dwMask = CFM_COLOR,              \
+SBCF(name).crTextColor = MKRGB(name))
+
+#define RGBOP DCLRGB
+
+#define DefHexRGB OPHEXRGB(RGBOP, _COMMA)
+#define DefCdlRGB OPCDLRGB(RGBOP, _COMMA)
+#define DefDbgRGB OPDBGRGB(RGBOP, _COMMA)
+#define DefDbgChFmt OPDBGRGB(SBCF, _COMMA)
+
+extern int DefHexRGB, DefCdlRGB, DefDbgRGB;
+
+#undef RGBOP
+#define RGBOP DEFRGB
+
+#define RestoreDefaultHexColor() (DefHexRGB)
+#define IsHexColorDefault() (!(OPHEXRGB(CMPRGB, ||)))
+
+#define RestoreDefaultCdlColor() (DefCdlRGB)
+#define IsCdlColorDefault() (!(OPCDLRGB(CMPRGB, ||)))
+
+#define InitDbgCharFormat() (OPDBGRGB(INITCF, _COMMA))
+#define RestoreDefaultDebugColor() (OPDBGRGB(DEFCF, _COMMA))
+#define IsDebugColorDefault() (!(OPDBGRGB(CMPCF, ||)))
 
 #endif

src/drivers/win/debuggersp.cpp
@@ -21,7 +21,6 @@
 #include "common.h"
 #include "utils/xstring.h"
 #include "debuggersp.h"
-#include "debugger.h"
 #include "../../fceu.h"
 #include "../../debug.h"
 #include "../../conddebug.h"

src/drivers/win/debuggersp.h
@@ -68,3 +68,6 @@ bool DoSymbolicDebugNaming(int offset, int size, HWND parentHWND);
 void AddNewSymbolicName(uint16 newAddress, char* newOffset, char* newName, char* newComment, int size, int init, bool nameOverwrite, bool commentHeadOnly, bool commentOverwrite);
 void DeleteSymbolicName(uint16 address, int size);
 void WriteNameFileToDisk(const char* filename, Name* node);
+
+extern void Disassemble(HWND hWnd, int id, int scrollid, unsigned int addr);
+extern void CenterWindow(HWND hwnd);

src/drivers/win/header_editor.cpp
@@ -1806,7 +1806,7 @@ bool GetComboBoxListItemData(HWND hwnd, UINT id, int* value, char* buf, iNES_HEA
 // Warning: when in save mode, the content of buf might be overwritten by the save filename which user changed.
 bool ShowINESFileBox(HWND parent, char* buf, iNES_HEADER* header)
 {
-	char *filename = NULL, *path = NULL;
+	char filename[2048] = { 0 }, path[2048] = { 0 };
 	bool success = true;
 
 	if (header)
@@ -1814,24 +1814,23 @@ bool ShowINESFileBox(HWND parent, char* buf, iNES_HEADER* header)
 		// When open this dialog for saving prpose, the buf must be a separate buf.
 		if (buf && buf != LoadedRomFName)
 		{
-			extern char* GetRomName(bool force = false);
-			extern char* GetRomPath(bool force = false);
-			filename = GetRomName(true);
+			extern std::string GetRomName(bool force = false);
+			extern std::string GetRomPath(bool force = false);
+			strcpy(filename, GetRomName(true).c_str());
 			char* second = strchr(filename, '|');
 			if (second)
 			{
-				char* _filename = (char*)calloc(1, 2048);
+				char _filename[2048];
 				strcpy(_filename, second + 1);
 				char* third = strrchr(filename, '\\');
 				if (third)
 					strcpy(_filename, third + 1);
-				free(filename);
-				filename = _filename;
+				strcpy(filename, _filename);
 			}
 			char header_str[32];
 			sprintf(header_str, " [%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X].nes", header->ROM_size, header->VROM_size, header->ROM_size, header->ROM_type2, header->ROM_type3, header->Upper_ROM_VROM_size, header->RAM_size, header->VRAM_size, header->TV_system, header->VS_hardware, header->reserved[0], header->reserved[1]);
 			strcat(filename, header_str);
-			path = GetRomPath(true);
+			strcpy(path, GetRomPath(true).c_str());
 		}
 		else
 			success = false;
@@ -1839,8 +1838,6 @@ bool ShowINESFileBox(HWND parent, char* buf, iNES_HEADER* header)
 	else {
 		if (!buf)
 			buf = LoadedRomFName;
-		filename = (char*)calloc(1, 2048);
-		path = (char*)calloc(1, 2048);
 	}
 
 	if (success)
@@ -1863,9 +1860,6 @@ bool ShowINESFileBox(HWND parent, char* buf, iNES_HEADER* header)
 		else
 			success = false;
 	}
-
-	if (filename) free(filename);
-	if (path) free(path);
 
 	return success;
 }

commit 56adae5e6ca9bcce47b675c419545adc837f2ec2

fixed copypaste bug in cdlogger introduced in #310 merge (cdl auto load were broken, an error occures while attempting to save changes to cdl window settings)

src/drivers/win/cdlogger.cpp
@@ -288,8 +288,8 @@ void SaveCDLogFile()
 	if (loadedcdfile[0] == 0)
 	{
 		char nameo[2048];
-		strcpy(nameo, GetRomName().c_str());
-		strcat(nameo, mass_replace(GetRomPath(), "|", ".").c_str());
+		strcpy(nameo, GetRomPath().c_str());
+		strcat(nameo, mass_replace(GetRomName(), "|", ".").c_str());
 		strcat(nameo, ".cdl");
 		RenameCDLog(nameo);
 	}
@@ -499,7 +499,7 @@ void CDLoggerROMChanged()
 
 	// try to load respective CDL file
 	char nameo[2048];
-	strcpy(nameo, GetRomName().c_str());
+	strcpy(nameo, GetRomPath().c_str());
 	strcat(nameo, mass_replace(GetRomName(), "|", ".").c_str());
 	strcat(nameo, ".cdl");
 