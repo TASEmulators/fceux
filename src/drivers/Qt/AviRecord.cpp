fb30f57beca7102e4061b884e4c536a21642c29f
mjbudd77 <mjbudd77@gmail.com>
2021-04-29 04:51:05 -0400

    Removed unistd.h include from file.

--- src/drivers/Qt/AviRecord.cpp
+++ src/drivers/Qt/AviRecord.cpp
@@ -10,6 +10,22 @@
 static gwavi_t  *gwavi = NULL;
 static bool      recordEnable = false;
 //**************************************************************************************
+
+static void convertRgb_32_to_24( const char *src, char *dest, int nPix )
+{
+	int i=0, j=0, s;
+
+	s = nPix * 4;
+
+	while ( i < s )
+	{
+		dest[j] = src[i]; i++; j++;
+		dest[j] = src[i]; i++; j++;
+		dest[j] = src[i]; i++; j++;
+		i++;
+	}
+}
+//**************************************************************************************
 int aviRecordOpenFile( const char *filepath, int format, int width, int height )
 {
 	if ( gwavi != NULL )
--- src/drivers/Qt/avi/gwavi.cpp
+++ src/drivers/Qt/avi/gwavi.cpp
@@ -36,7 +36,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 
 #include "gwavi.h"
