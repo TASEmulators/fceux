21b640a78f8070e5b6dcce416266437cc8d95317
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2021-06-08 12:51:03 +0300

    Minor fix to avoid warning

--- src/drivers/win/keyboard.h
+++ src/drivers/win/keyboard.h
@@ -1,6 +1,6 @@
 void KeyboardClose(void);
 int KeyboardInitialize(void);
-void KeyboardUpdate(void);
+void KeyboardUpdateState(void);
 unsigned int *GetKeyboard(void);
 unsigned int *GetKeyboard_nr(void);
 unsigned int *GetKeyboard_jd(void);
