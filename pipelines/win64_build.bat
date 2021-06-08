6be59b2d8d44d20ced73ed29c83b51cb748943b7
Brad Smith <bbbradsmith@users.noreply.github.com>
2021-06-08 17:43:13 -0400

    win64_build.bat should use 7z_64.dll

--- pipelines/win64_build.bat
+++ pipelines/win64_build.bat
@@ -9,7 +9,7 @@ cd %PROJECT_ROOT%\vc
 REM Create Zip Archive
 
 cd %PROJECT_ROOT%\output
-..\vc\zip -X -9 -j ..\vc\fceux64.zip ..\vc\x64\Release\fceux64.exe ..\src\drivers\win\lua\x64\lua5.1.dll ..\src\drivers\win\lua\x64\lua51.dll ..\src\auxlib.lua ..\src\drivers\win\7z.dll
+..\vc\zip -X -9 -j ..\vc\fceux64.zip ..\vc\x64\Release\fceux64.exe ..\src\drivers\win\lua\x64\lua5.1.dll ..\src\drivers\win\lua\x64\lua51.dll ..\src\auxlib.lua ..\src\drivers\win\7z_64.dll
 @if ERRORLEVEL 1 goto end
 ..\vc\zip -X -9 -u -r ..\vc\fceux64.zip fceux.chm taseditor.chm palettes luaScripts tools
 @if ERRORLEVEL 1 goto end
