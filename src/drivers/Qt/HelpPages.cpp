f8b6d19a94564d491ecbac932ab60aa8801d395e
mjbudd77 <mjbudd77@gmail.com>
2021-08-15 21:09:12 -0400

    Build fixes for Qt windows chm help file loader.

--- src/drivers/Qt/HelpPages.cpp
+++ src/drivers/Qt/HelpPages.cpp
@@ -2,20 +2,29 @@
 
 #include <string>
 
+#include "driver.h"
 #include "Qt/HelpPages.h"
+#include "Qt/ConsoleWindow.h"
 
 #ifdef WIN32
+#include <Windows.h>
 #include <htmlhelp.h>
 
 void OpenHelpWindow(std::string subpage)
 {
-	std::string helpFileName = BaseDirectory;
-	helpFileName += "\\fceux.chm";
+	HWND helpWin;
+	std::string helpFileName = FCEUI_GetBaseDirectory();
+	helpFileName += "\\..\\doc\\fceux.chm";
 	if (subpage.length() > 0)
 	{
 		helpFileName = helpFileName + "::/" + subpage + ".htm";
 	}
-	HtmlHelp(GetDesktopWindow(), helpFileName.c_str(), HH_DISPLAY_TOPIC, (DWORD)NULL);
+	//printf("Looking for HelpFile '%s'\n", helpFileName.c_str() );
+	helpWin = HtmlHelp( HWND(consoleWindow->winId()), helpFileName.c_str(), HH_DISPLAY_TOPIC, (DWORD)NULL);
+	if ( helpWin == NULL )
+	{
+		printf("Error: Failed to open help file '%s'\n", helpFileName.c_str() );
+	}
 }
 
 #else
