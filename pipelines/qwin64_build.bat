3262e182e86daadcac3ecc67cbeff377f43de259
mjbudd77 <mjbudd77@gmail.com>
2021-09-16 21:20:59 -0400

    Setup qt win64 pipeline to auto download libav from external site and link into program. Make linking libav optional in cmake for qt win64 build.

--- pipelines/qwin64_build.bat
+++ pipelines/qwin64_build.bat
@@ -22,18 +22,24 @@ cd    build
 mkdir bin
 
 curl -s -LO http://www.libsdl.org/release/SDL2-devel-2.0.14-VC.zip
+curl -s -LO https://github.com/GyanD/codexffmpeg/releases/download/4.4/ffmpeg-4.4-full_build-shared.zip
 
 REM rmdir /q /s SDL2
 
 powershell -command "Expand-Archive" SDL2-devel-2.0.14-VC.zip .
+powershell -command "Expand-Archive" ffmpeg-4.4-full_build-shared.zip
 
 rename SDL2-2.0.14  SDL2
+move   ffmpeg-4.4-full_build-shared\ffmpeg-4.4-full_build-shared   ffmpeg
+rmdir  ffmpeg-4.4-full_build-shared
+del    ffmpeg-4.4-full_build-shared.zip
 
 set SDL_INSTALL_PREFIX=%CD%
+set FFMPEG_INSTALL_PREFIX=%CD%
 
 REM cmake -h
 REM cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DSDL_INSTALL_PREFIX=%SDL_INSTALL_PREFIX%  ..
-cmake -DQT6=0 -DSDL_INSTALL_PREFIX=%SDL_INSTALL_PREFIX%  ..
+cmake -DQT6=0 -DSDL_INSTALL_PREFIX=%SDL_INSTALL_PREFIX%  -DUSE_LIBAV=1 -DFFMPEG_INSTALL_PREFIX=%FFMPEG_INSTALL_PREFIX%  ..
 
 REM nmake
 msbuild /m fceux.sln /p:Configuration=Release
@@ -41,6 +47,7 @@ msbuild /m fceux.sln /p:Configuration=Release
 
 copy src\Release\fceux.exe bin\qfceux.exe
 copy %SDL_INSTALL_PREFIX%\SDL2\lib\x64\SDL2.dll  bin\.
+copy %FFMPEG_INSTALL_PREFIX%\ffmpeg\bin\*.dll  bin\.
 
 windeployqt  --no-compiler-runtime  bin\qfceux.exe
 
