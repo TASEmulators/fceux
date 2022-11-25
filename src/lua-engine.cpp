542321dc3f0a33f11ed4106ebb2d28b48b2caad9
Artur Bekker <arturbekker42@gmail.com>
2022-11-25 17:10:26 -0500

    Update lua-engine.cpp
    Fixed the switchDisk and insertOrEject disk functions, to properly call the functions in input.cpp

--- src/lua-engine.cpp
+++ src/lua-engine.cpp
@@ -632,7 +632,7 @@ extern void ReloadRom(void);
 // Switches the side of the disk that is currently ejected
 static int emu_switchDisk(lua_State* L)
 {
-	extern void FCEUI_FDSSelect();
+	FCEUI_FDSSelect();
 	return 0;
 }
 
@@ -641,9 +641,10 @@ static int emu_switchDisk(lua_State* L)
 // If the disk is currently inserted into the FDS, eject it
 static int emu_insertOrEjectDisk(lua_State* L)
 {
-	extern void FCEUI_FDSInsert();
+	FCEUI_FDSInsert();
 	return 0;
 }
+
 
 // emu.loadrom(string filename)
 //
