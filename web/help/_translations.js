cd5253babeffda48c5b42085cdd7a0dfb8b91458
harry <hrosen2016@gmail.com>
2023-02-07 21:31:04 -0500

    Updated and regenerated help docs with new debugger.getsymboloffset lua info.

--- web/help/_translations.js
+++ web/help/_translations.js
@@ -2,7 +2,7 @@
 function hnd_ut(a){
 a.TRANSLATIONS['Search term too short'] = "Search term too short";
 a.TRANSLATIONS['No results'] = "No results";
-a.TRANSLATIONS['Please enter 3 or more characters'] = "Please enter 3 or more characters";
+a.TRANSLATIONS['Please enter more characters'] = "Please enter more characters";
 a.TRANSLATIONS['Word list not ready yet. Please wait until the word list is fully downloaded'] = "Word list not ready yet. Please wait until the word list is fully downloaded";
 a.TRANSLATIONS['Incorrect or corrupt search data. Please check your HelpNDoc template'] = "Incorrect or corrupt search data. Please check your HelpNDoc template";
 a.TRANSLATIONS['Related topics...'] = "Related topics...";
