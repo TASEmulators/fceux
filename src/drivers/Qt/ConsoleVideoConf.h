e608de906c299d2605d7828c0df8519b5f254a54
Andy Vandijck <andyvand@gmail.com>
2024-02-24 10:29:55 +0100

    Update ConsoleVideoConf.h

--- src/drivers/Qt/ConsoleVideoConf.h
+++ src/drivers/Qt/ConsoleVideoConf.h
@@ -55,7 +55,7 @@ class ConsoleVideoConfDialog_t : public QDialog
 		QCheckBox   *showFrameCount_cbx;
 		QCheckBox   *showLagCount_cbx;
 		QCheckBox   *showRerecordCount_cbx;
-		#ifdef WIN32
+		#if defined(WIN32) && (QT_VERSION_MAJOR < 6)
 		QCheckBox   *winFullScreenBorderCbx;
 		#endif
 		QDoubleSpinBox *xScaleBox;
